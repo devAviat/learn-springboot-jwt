3753b103bf2b2dbed8a70a2427e858541a5c8b70
adc_min <min0903@adcapsule.co.kr>
2021-04-01 10:40:25 +0900

    init commit

---
+++ jwt-security/src/main/java/com/learn/jwt/JwtMainApplication.java
@@ -0,0 +1,12 @@
+package com.learn.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtMainApplication.class, args);
+    }
+}
