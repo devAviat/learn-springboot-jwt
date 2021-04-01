f5a5db37cb24af1400594a2beeb5b85a631bd1f1
adc_min <min0903@adcapsule.co.kr>
2021-04-01 14:28:04 +0900

    - SecurityWebApplicationInitializer - WebSecurityConfig를 수퍼 클래스로 전달.

---
+++ spring-security/src/main/java/com/learn/security/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.learn.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityWebApplicationInitializer(){
+        super(WebSecurityConfig.class);
+    }
+
+}
