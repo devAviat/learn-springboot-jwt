0613442752c2a57fd1be3621e6335354bb12d456
adc_min <min0903@adcapsule.co.kr>
2021-04-01 14:43:59 +0900

    - spring MVC를 쓰는 환경이므로 수정필요.

--- spring-security/src/main/java/com/learn/security/config/SecurityWebApplicationInitializer.java
+++ spring-security/src/main/java/com/learn/security/config/SecurityWebApplicationInitializer.java
@@ -3,9 +3,4 @@ package com.learn.security.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-    public SecurityWebApplicationInitializer(){
-        super(WebSecurityConfig.class);
-    }
-
 }
