2d6c6e350a341e22fc1417650ab0c8bc590f3940
adc_min <min0903@adcapsule.co.kr>
2021-04-02 17:28:24 +0900

    - security config 수정.

--- spring-security/src/main/java/com/learn/security/config/MultiHttpSecurityConfig.java
+++ spring-security/src/main/java/com/learn/security/config/MultiHttpSecurityConfig.java
@@ -1,10 +1,17 @@
 package com.learn.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.*;
 
 @EnableWebSecurity
 public class MultiHttpSecurityConfig {
@@ -16,8 +23,36 @@ public class MultiHttpSecurityConfig {
 
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(user.username("user").password("password").roles("USER").build());
-        manager.createUser(user.username("admin").password("password").roles("USER","ADMIN").build());
+        manager.createUser(user.username("admin").password("password").roles("USER", "ADMIN").build());
 
         return manager;
     }
+
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdaptor extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**")
+                    .authorizeRequests(authorize -> authorize
+                            .anyRequest()
+                            .hasRole("ADMIN")
+                    )
+                    .httpBasic();
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class FormLoginWebSecurityConfigurerAdaptor extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authorize -> authorize
+                            .anyRequest()
+                            .authenticated()
+                    )
+                    .formLogin(withDefaults());
+        }
+    }
+
 }
